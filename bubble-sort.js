cee6029c0ed6b7a00a51d87faab764ab42cb29e1
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2021-04-06 12:18:09 +0300

    First commit

---
+++ bubble-sort.js
@@ -0,0 +1,66 @@
+// BubbleSort Pseudocode
+
+// Start looping from the end of the array towards the beginning with a variable called i
+// Start an inner loop with a variable called j from the begining until i - 1
+// If arr[j] is greater than arr[j+1], swap those two values
+// At the end return the sorted array
+
+const swap = (arr, idx1, idx2) => {
+	[ arr[idx1], arr[idx2] ] = [ arr[idx2], arr[idx1] ];
+};
+
+// Naive solution where we do not account of looping without including the last item which had bubbled to the top
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// SWAP
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+	return arr;
+}
+
+function bubbleSortBetter(arr) {
+	for (let i = arr.length; i > 0; i--) {
+		// Now that i is equal the arr length
+		// You can define j based on i
+		// This ensures no useless comparissons between values that were already sorted
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// SWAP
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+	return arr;
+}
+
+// Optimized for nearly sorted arrays - it won't continue comparing values if no swaps are done
+// If no swaps are done it means the array is already sorted
+function bubbleSortBetterOptimized(arr) {
+	let noSwaps;
+	for (let i = arr.length; i > 0; i--) {
+		noSwaps = true;
+		// Now that i is equal the arr length
+		// You can define j based on i
+		// This ensures no useless comparissons between values that were already sorted
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// SWAP
+				swap(arr, j, j + 1);
+				noSwaps = false;
+			}
+		}
+		if (noSwaps) break;
+	}
+	return arr;
+}
+
+console.log(bubbleSort([ 8, 1, 2, 3, 4, 5, 6, 7 ]));
+console.log(bubbleSortBetter([ 8, 1, 2, 3, 4, 5, 6, 7 ]));
+console.log(bubbleSortBetterOptimized([ 8, 1, 2, 3, 4, 5, 6, 7 ]));
+// In oposite direction the noSwap method is not as usefull
+// Would need to also loop differently for this to work well on the below almost sorted array
+console.log(bubbleSortBetterOptimized([ 2, 3, 4, 5, 6, 7, 8, 1 ]));
